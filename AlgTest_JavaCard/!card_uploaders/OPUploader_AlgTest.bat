c7a7a6f516a9981d06d20ad9da3853c4f62ee7f6
PetrS <petr@0x9000.org>
2013-06-28 10:49:40 +0200

    cleanup

--- AlgTest_JavaCard/!card_uploaders/OPUploader_AlgTest.bat
+++
@@ -1,1 +0,0 @@
-OPUploader.exe /packageID:"6D 79 70 61 63 6B 61 67 31" /appletID:"6D 79 70 61 63 30 30 30 31" /appletPath:"D:\Documents\Develop\AlgTest\cyberflex\AlgTest\javacard\AlgTest.ijc" /staticSize:"30" /instanceSize:"6000" /installParams:"00" 
