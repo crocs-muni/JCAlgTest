5cf6666fc99e51edd8c6afd9026c894b93cf5de0
petrs <petrsgit@gmail.com>
2016-04-27 16:41:25 +0200

    moved bat for creating html results

---
+++ AlgTest_Process/run.bat
@@ -0,0 +1,8 @@
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\Profiles HTML
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\Profiles\performance\fixed\ JCINFO
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\Profiles\performance\variable\ GRAPHSPAGE
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\Profiles\performance\fixed\ SORTABLE
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\Profiles\performance\fixed\ COMPAREGRAPH
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\Profiles\performance\fixed\ COMPARETABLE
+
+
--- run.bat
+++
@@ -1,8 +0,0 @@
-java -jar ..\AlgTest\AlgTest_Process\dist\AlgTestProcess.jar ..\AlgTest\Profiles HTML
-java -jar ..\AlgTest\AlgTest_Process\dist\AlgTestProcess.jar ..\AlgTest\Profiles\performance\fixed\ JCINFO
-java -jar ..\AlgTest\AlgTest_Process\dist\AlgTestProcess.jar ..\AlgTest\Profiles\performance\variable\ GRAPHSPAGE
-java -jar ..\AlgTest\AlgTest_Process\dist\AlgTestProcess.jar ..\AlgTest\Profiles\performance\fixed\ SORTABLE
-java -jar ..\AlgTest\AlgTest_Process\dist\AlgTestProcess.jar ..\AlgTest\Profiles\performance\fixed\ COMPAREGRAPH
-java -jar ..\AlgTest\AlgTest_Process\dist\AlgTestProcess.jar ..\AlgTest\Profiles\performance\fixed\ COMPARETABLE
-
-
