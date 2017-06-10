02c26082476a17b5fc2583b710d5d8442bab10a6
petrs <petrsgit@gmail.com>
2017-06-10 14:19:57 +0200

    fixed incorrect order of values for KeyAgreement

--- AlgTest_JClient/src/algtestjclient/SingleModeTest.java
+++ AlgTest_JClient/src/algtestjclient/SingleModeTest.java
@@ -125,10 +125,14 @@ public class SingleModeTest {
 
     public static final String KEYAGREEMENT_STR[] = {"javacard.security.KeyAgreement", 
         //2.2.1
-        "ALG_EC_SVDP_DH#2.2.1", "ALG_EC_SVDP_DHC#2.2.1",
+        "ALG_EC_SVDP_DH/ALG_EC_SVDP_DHC_KDF#2.2.1", "ALG_EC_SVDP_DHC/ALG_EC_SVDP_DHC_KDF#2.2.1",
         //3.0.1
-        "ALG_EC_SVDP_DH_KDF#3.0.1", "ALG_EC_SVDP_DH_PLAIN#3.0.1", "ALG_EC_SVDP_DHC_KDF#3.0.1", "ALG_EC_SVDP_DHC_PLAIN#3.0.1"
+        //was like this: "ALG_EC_SVDP_DH_KDF#3.0.1", "ALG_EC_SVDP_DH_PLAIN#3.0.1", "ALG_EC_SVDP_DHC_KDF#3.0.1", "ALG_EC_SVDP_DHC_PLAIN#3.0.1",
+        "ALG_EC_SVDP_DH_PLAIN#3.0.1", "ALG_EC_SVDP_DHC_PLAIN#3.0.1", 
+        //3.0.5    
+        "ALG_EC_PACE_GM#3.0.5", "ALG_EC_SVDP_DH_PLAIN_XY#3.0.5", "ALG_DH_PLAIN#3.0.5"
     };
+    public static final int KEYAGREEMENT_STR_LAST_INDEX = JCConsts.KeyAgreement_ALG_DH_PLAIN;
 /*
     public static final String KEYBUILDER_STR[] = {
         "javacard.security.KeyBuilder", 
