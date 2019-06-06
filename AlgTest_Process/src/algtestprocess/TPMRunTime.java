61bced5063ce8610af2750d9cdb6aa36e23798d4
Simon Struk <simon.struk@gmail.com>
2019-06-06 22:27:09 +0200

    Add TPM Run Time.

--- AlgTest_Process/src/algtestprocess/TPMRunTime.java
+++ AlgTest_Process/src/algtestprocess/TPMRunTime.java
@@ -2,12 +2,15 @@ package algtestprocess;
 
 import static algtestprocess.JCinfohtml.*;
 import static algtestprocess.TPMSupportTable.TPM2_ECC_CURVE_STR;
+import static algtestprocess.TPMSupportTable.TPM2_ALG_ID_STR;
 import static java.lang.System.out;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.io.File;
 import java.util.HashSet;
 import java.util.HashMap;
@@ -15,7 +18,6 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class TPMRunTime {
-    static final String TABLE_HEAD = "<table cellspacing='0'> <!-- cellspacing='0' is important, must stay -->\n\t<tr><th style=\"width: 330px;\">Key parameters</th><th><b>Operation average (ms/op)</b></th><th>Operation minimum (ms/op)</th><th>Operation maximum (ms/op)</th><th></th><th class=\"minor\">Iterations</th><th class=\"minor\">Successful</th><th class=\"minor\">Failed</th></tr><!-- Table Header -->\n";
 
     public static void beginRunTimeHTML(FileOutputStream file, String title) throws IOException {
         String toFile = "";
@@ -68,24 +70,65 @@ public class TPMRunTime {
         String html = "";
         String line;
         String[] fields;
+        String command = "";
         boolean inTable = false;
+        boolean createHeader = false;
         while ((line = reader.readLine()) != null) {
             if (line.startsWith("TPM2_")) { // new command
+                command = line;
                 if (inTable) {
                     html += "</table>\n</br>\n";
                 }
                 inTable = true;
+                createHeader = true;
                 html += "<h3 id=\"" + line + "\">" + line + "</h3>\n";
-                html += TABLE_HEAD;
-            } else if (line.startsWith("key params:")) {
+                html += "<table cellspacing='0'> <!-- cellspacing='0' is important, must stay -->\n\t<tr>";
+            } else {
                 fields = line.trim().split(";");
-                assert fields.length >= 2;
-                if (fields[1].contains("ECC")) {
-                    int ecc_index = fields[1].indexOf("0x");
-                    int curve_id = Integer.parseInt(fields[1].substring(ecc_index + 2), 16);
-                    fields[1] = fields[1].substring(0, ecc_index) + TPM2_ECC_CURVE_STR.get(curve_id).substring(9); // skip TMP2_ECC_
+                if (fields.length < 2) continue;
+
+                if (createHeader) {
+                    for (int i = 0; i < fields.length; i += 2) {
+                        html += "<th style=\"width: 330px;\">"+fields[i]+"</th>";
+                    }
+                    html += "<th></th><th>Operation average (ms/op)</th><th>Operation minimum (ms/op)</th><th>Operation maximum (ms/op)</th>";
+                    html += "<th></th><th class=\"minor\">Iterations</th><th class=\"minor\">Successful</th><th class=\"minor\">Failed</th><th class=\"minor\">Error code</th></tr><!-- Table Header -->\n";
+                    createHeader = false;
                 }
-                html += "\t<tr><td><b>" + fields[1] + "</b></td>";
+
+                if (Arrays.asList(new String[] {
+                        "TPM2_Create", "TPM2_RSA_Encrypt", "TPM2_RSA_Decrypt", "TPM2_Sign", "TPM2_VerifySignature"
+                        }).contains(command)) {
+                    /* Key parameters */
+                    if (fields[1].contains("ECC")) {
+                        int eccIndex = fields[1].indexOf("0x");
+                        int curveId = Integer.parseInt(fields[1].substring(eccIndex + 2), 16);
+                        fields[1] = fields[1].substring(0, eccIndex) + TPM2_ECC_CURVE_STR.get(curveId).substring(9); // skip TMP2_ECC_
+                    } else if (fields[1].contains("SYMCIPHER") || fields[1].contains("KEYEDHASH")) {
+                        int hexIndex = fields[1].indexOf("0x");
+                        int algId = Integer.parseInt(fields[1].substring(hexIndex + 2, hexIndex + 6), 16);
+                        fields[1] = fields[1].replace(fields[1].substring(hexIndex, hexIndex+6), TPM2_ALG_ID_STR.get(algId).substring(9));
+                    }
+                    if (!command.equals("TPM2_Create")) {
+                        /* Scheme */
+                        int algId = Integer.parseInt(fields[3].substring(2), 16);
+                        fields[3] = TPM2_ALG_ID_STR.get(algId).substring(9);
+                    }
+                } else if (command.equals("TPM2_EncryptDecrypt")) {
+                    int algId = Integer.parseInt(fields[1].substring(2), 16);
+                    fields[1] = TPM2_ALG_ID_STR.get(algId).substring(9);
+                    algId = Integer.parseInt(fields[5].substring(2), 16);
+                    fields[5] = TPM2_ALG_ID_STR.get(algId).substring(9);
+                } else if (command.equals("TPM2_Hash")) {
+                    int algId = Integer.parseInt(fields[1].substring(2), 16);
+                    fields[1] = TPM2_ALG_ID_STR.get(algId).substring(9);
+                }
+
+                html += "\t<tr>";
+                for (int i = 1; i < fields.length; i += 2) {
+                    html += "<td><b>" + fields[i] + "</b></td>";
+                }
+                html += "<td></td>";
 
                 line = reader.readLine();
                 assert line != null;
@@ -101,9 +144,10 @@ public class TPMRunTime {
                 assert line.startsWith("operation info:");
                 fields = line.trim().split(";");
                 assert fields.length >= 6;
-                html += "<td class=\"minor\"></td><td class=\"minor\">" + Integer.valueOf(fields[2]) + "</td>";
-                html += "<td class=\"minor\">" + Integer.valueOf(fields[4]) + "</td>";
-                html += "<td class=\"minor\">" + Integer.valueOf(fields[6]) + "</td>";
+                html += "<td class=\"minor\"></td><td class=\"minor\">" + fields[2] + "</td>";
+                html += "<td class=\"minor\">" + fields[4] + "</td>";
+                html += "<td class=\"minor\">" + fields[6] + "</td>";
+                html += "<td class=\"minor\">" + fields[8] + "</td>";
                 html += "</tr>\n";
             }
         }
@@ -130,6 +174,7 @@ public class TPMRunTime {
         html += "<p>Manufacturer: <strong>" + infoMap.get("Manufacturer") + "</strong></p>\n";
         html += "<p>Vendor string: <strong>" + infoMap.get("Vendor string") + "</strong></p>\n";
         html += "<p>Firmware version: <strong>" + infoMap.get("Firmware version") + "</strong></p>\n";
+        html += "<p>Algtest version: <strong>" + infoMap.get("Image tag") + "</strong></p>\n";
 
         html += "</div>";
         output.write(html.getBytes());
@@ -162,9 +207,11 @@ public class TPMRunTime {
     public static List<String> generateRunTimeFolder(String dir) throws IOException {
         List<String> files = listFilesForFolder(new File(dir));
         List<String> names = new ArrayList<>();
+        Collections.sort(files);
 
-        for(String filePath : files)
+        for(String filePath : files) {
             names.add(generateRunTimeFile(filePath));
+        }
 
         return names;
     }
