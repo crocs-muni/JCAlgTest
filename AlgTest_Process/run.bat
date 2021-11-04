f6b6d1a55dc6e0b1c664f240814bd4a4194db7a3
Petr Svenda <petrsgit@gmail.com>
2021-11-04 09:51:56 +0100

    fix path in process data script

--- AlgTest_Process/run.bat
+++ AlgTest_Process/run.bat
@@ -1,10 +1,10 @@
 java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles HTML
 
-java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles_20211030\performance\fixed\ SIMILARITY ..\..\algtest_results\web1\
-java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles_20211030\performance\fixed\ JCINFO ..\..\algtest_results\web1\
-java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles_20211030\performance\fixed\ SORTABLE ..\..\algtest_results\web1\
-java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles_20211030\performance\fixed\ RADAR ..\..\algtest_results\web1\
-java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles_20211030\performance\variable\ SCALABILITY ..\..\algtest_results\web1\
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles\performance\fixed\ SIMILARITY ..\..\algtest_results\javacard\web\
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles\performance\fixed\ JCINFO ..\..\algtest_results\javacard\web\
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles\performance\fixed\ SORTABLE ..\..\algtest_results\javacard\web\
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles\performance\fixed\ RADAR ..\..\algtest_results\javacard\web\
+java -jar ..\AlgTest_Process\dist\AlgTestProcess.jar ..\..\algtest_results\javacard\Profiles\performance\variable\ SCALABILITY ..\..\algtest_results\javacard\web\
 
 
 
